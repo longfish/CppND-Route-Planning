07537e24e1ec615a977c75e4e394008eee730900
longfish <loongfish@foxmail.com>
2021-10-02 19:19:51 -0700

    finished the TODO 1, begin and end point

--- src/main.cpp
+++ src/main.cpp
@@ -55,9 +55,9 @@ int main(int argc, const char **argv) {
   // user input for these values using std::cin. Pass the user input to the
   // RoutePlanner object below in place of 10, 10, 90, 90.
   float start_x, start_y, end_x, end_y;
-  std::cout << "Please input the start point (e.g. 10 10): ";
+  std::cout << "Start point (e.g. 10 10): ";
   std::cin >> start_x >> start_y;
-  std::cout << "And the end point (e.g. 90 90): ";
+  std::cout << "End point (e.g. 90 90): ";
   std::cin >> end_x >> end_y;
 
   // Build Model.
